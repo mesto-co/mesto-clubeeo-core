dbe14ab4c3d5e65594ec15fc07f331a66f7aaa13
Roman Ekzempliarov <r.exemplarov@gmail.com>
2024-11-04 15:18:21 +0400

    move mercurius to project & upgrade core dep

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,34 @@
-import {router} from 'clubeeo-core';
+import {router, graphqlResolvers, graphqlSchema} from 'clubeeo-core';
 import {MestoApp} from './App';
+import mercurius from 'mercurius';
+import mercuriusAuth from 'mercurius-auth';
+
+export const graphqlLoaders = (app: MestoApp) => ({
+});
 
 export function mestoRouter(app: MestoApp) {
   const r = router(app);
+
+  r.register(mercurius, {
+    schema: graphqlSchema,
+    resolvers: graphqlResolvers(app as any),
+    loaders: graphqlLoaders(app as any),
+    graphiql: true,
+    // // Add this configuration to disable federation features
+    // federationMetadata: false
+  });
+
+  r.register(mercuriusAuth, {
+    authContext: async (context) => {
+      return {
+        ctx: app.contexts.auth(context.reply.request),
+      }
+    },
+    async applyPolicy (authDirectiveAST, parent, args, context, info) {
+      return Boolean(await context.auth.getUser());
+    },
+    authDirective: 'auth'
+  });
 
   console.log(app.Env.nodeEnv)
   if (app.Env.nodeEnv === 'development') {
