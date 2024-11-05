a9d425e1d794f2c6a940a82df8e034fe034eaa8e
Roman Ekzempliarov <r.exemplarov@gmail.com>
2024-11-05 15:33:03 +0400

    use Apollo graphql

--- src/router.ts
+++ src/router.ts
@@ -1,33 +1,69 @@
-import {router, graphqlResolvers, graphqlSchema} from 'clubeeo-core';
+import { Query } from './../node_modules/@apollo/server/src/plugin/schemaReporting/generated/operations.d';
+import {router, graphqlResolvers, graphqlSchema, Club} from 'clubeeo-core';
 import {MestoApp} from './App';
-import mercurius from 'mercurius';
-import mercuriusAuth from 'mercurius-auth';
+import { ApolloServer } from '@apollo/server';
+import { fastifyApolloHandler } from '@as-integrations/fastify';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { ICtx } from 'clubeeo-core/dist/graphql/graphqlCommon';
 
 export const graphqlLoaders = (app: MestoApp) => ({
 });
 
-export function mestoRouter(app: MestoApp) {
+export async function mestoRouter(app: MestoApp) {
   const r = router(app);
 
-  r.register(mercurius, {
-    schema: graphqlSchema,
-    resolvers: graphqlResolvers(app as any),
-    loaders: graphqlLoaders(app as any),
-    graphiql: true,
-    // // Add this configuration to disable federation features
-    // federationMetadata: false
+  const resolvers = graphqlResolvers(app) as any;
+
+  const schema = makeExecutableSchema({
+    typeDefs: graphqlSchema,
+    resolvers: graphqlResolvers(app),
   });
 
-  r.register(mercuriusAuth, {
-    authContext: async (context) => {
+  // Create Apollo Server instance
+  const apollo = new ApolloServer<{app: MestoApp, auth: any}>({
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer: r.server })
+    ],
+    formatError: (error) => {
+      // Log the error using the app logger
+      app.logger.error({
+        err: error,
+        path: error.path,
+        code: error.extensions?.code,
+      }, 'GraphQL Error');
+
+      // Return a sanitized error message to the client
       return {
-        ctx: app.contexts.auth(context.reply.request),
+        message: app.env.nodeEnv === 'development' ? error.message : 'Internal server error',
+        path: error.path,
+        extensions: {
+          code: error.extensions?.code || 'INTERNAL_SERVER_ERROR'
+        }
+      };
+    },
+  });
+
+  // Start Apollo Server
+  await apollo.start();
+
+  // Register Apollo middleware
+  r.route({
+    method: ['GET', 'POST', 'OPTIONS'],
+    url: '/graphql',
+    handler: fastifyApolloHandler(apollo, {
+      context: async (request, reply) => {
+        const auth = app.contexts.auth(request as any);
+        return {
+          app,
+          auth: {
+            // todo: get rid of extra nesting
+            ctx: auth,
+          },
+        }
       }
-    },
-    async applyPolicy (authDirectiveAST, parent, args, context, info) {
-      return Boolean(await context.auth.getUser());
-    },
-    authDirective: 'auth'
+    })
   });
 
   console.log(app.Env.nodeEnv)
